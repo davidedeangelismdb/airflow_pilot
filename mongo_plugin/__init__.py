5841999d4b4154885aa9e24fed7e825df7a5f6b9
davidedeangelismdb <davide.deangelis@mongodb.com>
2018-09-06 14:48:42 +0100

    fixed import of deleted operator

--- mongo_plugin/__init__.py
+++ mongo_plugin/__init__.py
@@ -1,15 +1,15 @@
 from airflow.plugins_manager import AirflowPlugin
 from mongo_plugin.hooks.mongo_hook import MongoHook
-from mongo_plugin.operators.mongo_operator import MongoOperator
-from mongo_plugin.operators.findworflowdocs_operator import FindWorkflowsDocsOperator
 from mongo_plugin.operators.get_workflow import GetWorkflowOperator
 from mongo_plugin.operators.get_workflow_docs import GetWorkflowDocsOperator
-
+from mongo_plugin.operators.transform_doc_to_task import TransformDocToTaskOperator
 
 
 class MongoPlugin(AirflowPlugin):
     name = "MongoPlugin"
-    operators = [MongoOperator, FindWorkflowsDocsOperator, GetWorkflowOperator, GetWorkflowDocsOperator]
+    operators = [GetWorkflowOperator,
+                 GetWorkflowDocsOperator,
+                 TransformDocToTaskOperator]
     hooks = [MongoHook]
     executors = []
     macros = []
